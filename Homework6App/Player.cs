8e643cf06645acfba444fe7a138fa5f467378d45
aburlaka2 <aburlaka1@gmail.com>
2020-11-25 16:53:04 +0200

    Update Player.cs

--- Homework6App/Player.cs
+++ Homework6App/Player.cs
@@ -6,5 +6,20 @@ namespace Homework6App
 {
     public class Player
     {
+        public char Char { get; set; }
+        public int xPos { get; set; }
+        public int yPos { get; set; }
+        public bool isAlive { get; set; }
+        public Player()
+        {
+            Char = '☻';
+            xPos = 0;
+            yPos = 0;
+            isAlive = true;
+        }
+        public void GetInfo()
+        {
+            Console.WriteLine(xPos);
+        }
     }
 }
