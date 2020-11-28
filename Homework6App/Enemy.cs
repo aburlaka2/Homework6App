6e352cbcb975fd995928b17cd72752a22f47f2a2
aburlaka2 <aburlaka1@gmail.com>
2020-11-28 21:25:29 +0200

    Update Enemy.cs

--- Homework6App/Enemy.cs
+++ Homework6App/Enemy.cs
@@ -9,6 +9,10 @@ namespace Homework6App
         public char Char;
         public int xPos { get; set; }
         public int yPos { get; set; }
+        public char enemy { get; set; }
+
+        public int newPosX { get; set; }
+        public int newPosY { get; set; }
 
         public Enemy()
         {
@@ -28,89 +32,93 @@ namespace Homework6App
                 {
                     case 1:
                         {
-                            if (IsEnemyPositionAvailable(Map, xPos, yPos, 0, -1))
+                            if (IsEnemyPositionAvailable(Map, 0, -1))
                             {
-                                SetEnemyToPosition(Map, xPos, yPos, 0, -1);
+                                SetEnemyToPosition(Map, 0, -1);
+                                break;
                             }
                             else
                             {
                                 EnemyDirection = 0;
+                                continue;
                             }
-                            break;
+                            
                         }
                     case 2:
                         {
 
-                            if (IsEnemyPositionAvailable(Map, xPos, yPos, -1, 0))
+                            if (IsEnemyPositionAvailable(Map, -1, 0))
                             {
-                                SetEnemyToPosition(Map, xPos, yPos, -1, 0);
+                                SetEnemyToPosition(Map, -1, 0);
+                                break;
                             }
                             else
                             {
                                 EnemyDirection = 0;
+                                continue;
                             }
-                            break;
+                            
                         }
                     case 3:
                         {
 
-                            if (IsEnemyPositionAvailable(Map, xPos, yPos, 0, 1))
+                            if (IsEnemyPositionAvailable(Map, 0, 1))
                             {
-                                SetEnemyToPosition(Map, xPos, yPos, 0, 1);
+                                SetEnemyToPosition(Map, 0, 1);
+                                break;
                             }
                             else
                             {
                                 EnemyDirection=0;
+                                continue;
                             }
-                            break;
+                            
                         }
                     case 4:
                         {
 
-                            if (IsEnemyPositionAvailable(Map, xPos, yPos, 1, 0))
+                            if (IsEnemyPositionAvailable(Map, 1, 0))
                             {
-                                SetEnemyToPosition(Map, xPos, yPos, 1, 0);
+                                SetEnemyToPosition(Map, 1, 0);
+                                break;
                             }
                             else
                             {
                                 EnemyDirection=0;
+                                continue;
                             }
-                            break;
-                        }
-                    default:
-                        {
-                            break;
+                            
                         }
                 }
             
             }
-            static bool IsEnemyPositionAvailable(Cell[,]Map, int xPos, int yPos, int xDelta, int yDelta)
-            {
-                bool isInBounds = true;
-
-                int newPosX = xPos + xDelta;
-                int newPosY = yPos + yDelta;
-
-                if (xPos + xDelta < 0 || xPos + xDelta >= Map.GetLength(0) ||
-                    yPos + yDelta < 0 || yPos + yDelta >= Map.GetLength(1))
-                {
-                    isInBounds = false;
-                }
-                return isInBounds && Map[newPosX, newPosY].CanMove;
-            }
-            static void SetEnemyToPosition(Cell[,]Map, int xPos, int yPos, int xDelta, int yDelta)
-            {
-                int newPosX = xPos + xDelta;
-                int newPosY = yPos + yDelta;
-
-                Map[xPos, yPos].Reset();
-                Map[newPosX, newPosY].SetEnemy('☻');
-
-                xPos = newPosX;
-                yPos = newPosY;
-
-            }
+            
         }
-    
+        public bool IsEnemyPositionAvailable(Cell[,] Map, int xDelta, int yDelta)
+        {
+            bool isInBounds = true;
+            int count = 1;
+
+            int newPosX = xPos + xDelta;
+            int newPosY = yPos + yDelta;
+
+            if (xPos + xDelta < 0 || xPos + xDelta >= Map.GetLength(0) ||
+                yPos + yDelta < 0 || yPos + yDelta >= Map.GetLength(1))
+            {
+                isInBounds = false;
+            }
+            return isInBounds && Map[newPosX, newPosY].CanMove;
+        }
+        public void SetEnemyToPosition(Cell[,] Map, int xDelta, int yDelta)
+        {
+            int newPosX = xPos + xDelta;
+            int newPosY = yPos + yDelta;
+
+            Map[xPos, yPos].ResetEnemy();
+            Map[newPosX, newPosY].SetEnemy(this.Char);
+
+            xPos = newPosX;
+            yPos = newPosY;
+        }
     }
 }
